749e6b60a612b2b1349cf60d2a5dd3904c1502e0
jakes670 <31097840+jakes670@users.noreply.github.com>
2021-02-07 19:15:03 +0200

    Create purge.sh

---
+++ purge.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/libexec/piler:/usr/local/libexec/piler
+PRIORITY=mail.error
+TMPFILE=/var/run/piler/purge.tmp
+PURGE_BEACON=/var/piler/stat/purge
+
+if [ -f $TMPFILE ]; then exit 1; fi
+
+date > $TMPFILE
+
+function finish {
+   rm -f $TMPFILE
+}
+
+trap finish EXIT
+
+touch $PURGE_BEACON
+
+pilerpurge-jl.py
